bfde8d7ed406820da8028b2ab10ad8a2aaf31cf3
Петя <petr@example.com>
2012-10-01 15:00:00 +0400

    API 1.0

--- project.sh
+++ project.sh
@@ -2,6 +2,20 @@
 
 param=1
 
+# API
+api_functionK (){
+    local tempk=$param
+    param=$tempk
+}
+api_functionL (){
+    local templ=$param
+    param=$tempI
+}
+api_functionM (){
+    local tempm=$param
+    param=$tempm
+}
+
 # MAIN
 main () {
     if [ "$param" == 1 ]
